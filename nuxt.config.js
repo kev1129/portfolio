ce79c12abec78e63a8a659a19f7b2141b43e60a0
kev1129 <shinado1129@gmail.com>
2019-05-16 22:04:48 +0900

    .

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,13 +11,14 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'description', content: '' },
-      { name: 'og:url', content: '' },
-      { name: 'og:title', content: '' },
-      { name: 'og:description', content: '' },
+      { name: 'title', content: 'Kebukawa Shinado Portfolio' },
+      { name: 'description', content: '毛部川 風のWebエンジニアとしてのポートフォリオサイトです。' },
+      { name: 'og:url', content: 'https://shinadokebukawa.com/portfolio/' },
+      { name: 'og:title', content: 'Kebukawa Shinado Portfolio' },
+      { name: 'og:description', content: '毛部川 風のWebエンジニアとしてのポートフォリオサイトです。' },
       { name: 'og:image', content: 'img/icon.jpg' },
-      { name: 'og:site_name', content: '' },
-      { name: 'twitter:site', content: '' },
+      { name: 'og:site_name', content: 'Kebukawa Shinado Portfolio' },
+      { name: 'twitter:site', content: 'Kebukawa Shinado Portfolio' },
       { name: 'og:locale', content: 'ja_JP' },
     ],
     link: [
@@ -41,7 +42,6 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    // '~/plugins/vue-scrollto.js'
   ],
 
   /*
